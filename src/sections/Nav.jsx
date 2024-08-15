101c0fc06d706954fb9e08aaa6467f034bb4775b
VictorVazquez512 <victorhugo.vazquezrojas@gmail.com>
2024-08-14 17:05:44 -0700

    Checking Prettier

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -3,8 +3,8 @@ import { hamburger } from "../assets/icons";
 
 const Nav = () => {
   return (
-    <header class="padding-x py-8 absolute z-10 w-full text-wrap">
-      <nav class="bg-black px-4 mx-auto container">
+    <header class="padding-x absolute z-10 w-full text-wrap py-8">
+      <nav class="container mx-auto bg-black px-4">
         <a href="/">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
